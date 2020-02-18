354eb5c61987ab0fabfe7c761e3bd4374900d1c5
DogSniffers <DogSniffers@users.noreply.github.com>
2020-02-18 16:04:35 -0700

    Register works

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express');
+const massive = require('massive');
+const session = require('express-session');
+const {CONNECTION_STRING,SESSION_SECRET} = process.env;
+const PORT = 4000;
+const authCtrl = require('./controllers/authController')
+
+const app = express();
+app.use(express.json());
+
+massive(CONNECTION_STRING).then(db =>{
+    app.set('db',db)
+    console.log('DB CONNECTED')
+})
+app.use(session({
+    resave:true,
+    saveUninitialized:false,
+    secret: SESSION_SECRET
+}))
+
+app.post('/auth/register', authCtrl.register);
+
+app.listen(PORT,() =>{
+    console.log(`I am listening on ${PORT}`)
+})
